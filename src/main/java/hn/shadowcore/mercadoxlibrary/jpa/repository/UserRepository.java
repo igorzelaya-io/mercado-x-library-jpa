d8114acac9816bade43a37922e827146bcbc4d67
Igor A. Zelaya <izelaya@baas.hn>
2025-07-09 15:21:06 -0600

    develp: Updated UserRepository

--- src/main/java/hn/shadowcore/mercadoxlibrary/jpa/repository/UserRepository.java
+++ src/main/java/hn/shadowcore/mercadoxlibrary/jpa/repository/UserRepository.java
@@ -10,5 +10,7 @@ import java.util.UUID;
 public interface UserRepository extends BaseRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 
+    Optional<User> findByIdAndEnabled(UUID uuid, boolean enabled);
+
     Optional<User> findByEmail(String email);
 }
